7f49461b3a136d6564a6254e52723655c7167974
MrMasrozYTLIVE <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2024-09-21 22:30:36 +0300

    I don't even know what to say here now because @TheFirstNoob rewrote tests

--- tests.py
+++ tests.py
@@ -1,25 +1,30 @@
+from __future__ import annotations
+
+import sys
 import unittest
+import json
+import asyncio
+
+from asgiref.sync import sync_to_async
 from g4f.client import Client
 from g4f.Provider import (
     AiChatOnline, AiChats, Blackbox, Airforce, Bixin123, Binjie, CodeNews, ChatGot, Chatgpt4o, ChatgptFree,
-    Chatgpt4Online,
-    DDG, DeepInfra, DeepInfraImage, FreeChatgpt, FreeGpt, Free2GPT, FreeNetfly, Koala, HuggingChat, HuggingFace, Nexra,
-    ReplicateHome, Liaobots, LiteIcoding, MagickPen, Prodia, PerplexityLabs, Pi, TeachAnything, TwitterBio, Snova, You,
-    Pizzagpt, RetryProvider
+    Chatgpt4Online, DDG, DeepInfra, DeepInfraImage, FreeChatgpt, FreeGpt, Free2GPT, FreeNetfly, Koala, HuggingChat,
+    HuggingFace, Nexra, ReplicateHome, Liaobots, LiteIcoding, MagickPen, Prodia, PerplexityLabs, Pi, TeachAnything,
+    TwitterBio, Snova, You, Pizzagpt, RetryProvider
 )
 
 
-class AITests(unittest.TestCase):
-    def test_provider_availability(self):
-        providers = {
-            "gpt-3.5-turbo": RetryProvider([FreeChatgpt, FreeNetfly, Bixin123, Nexra, TwitterBio, Airforce],
-                                           shuffle=False),
-            "gpt-4": RetryProvider([Chatgpt4Online, Nexra, Binjie, FreeNetfly, AiChats, Airforce, You, Liaobots],
-                                   shuffle=False),
+class AITests(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.INFO = '\x1b[34;1m'
+        self.ERROR = '\x1b[31m'
+
+        self.providers = {
+            "gpt-3.5-turbo": RetryProvider([FreeChatgpt, FreeNetfly, Bixin123, Nexra, TwitterBio, Airforce], shuffle=False),
+            "gpt-4": RetryProvider([Chatgpt4Online, Nexra, Binjie, FreeNetfly, AiChats, Airforce, You, Liaobots], shuffle=False),
             "gpt-4-turbo": RetryProvider([Nexra, Bixin123, Airforce, You, Liaobots], shuffle=False),
-            "gpt-4o-mini": RetryProvider(
-                [Pizzagpt, AiChatOnline, ChatgptFree, CodeNews, You, FreeNetfly, Koala, MagickPen, Airforce, DDG,
-                 Liaobots], shuffle=False),
+            "gpt-4o-mini": RetryProvider([Pizzagpt, AiChatOnline, ChatgptFree, CodeNews, You, FreeNetfly, Koala, MagickPen, Airforce, DDG, Liaobots], shuffle=False),
             "gpt-4o": RetryProvider([Chatgpt4o, LiteIcoding, AiChatOnline, Airforce, You, Liaobots], shuffle=False),
             "claude-3-haiku": RetryProvider([DDG, Liaobots], shuffle=False),
             "blackbox": RetryProvider([Blackbox], shuffle=False),
@@ -27,54 +32,70 @@ class AITests(unittest.TestCase):
             "gemini-pro": RetryProvider([ChatGot, Liaobots], shuffle=False),
             "gemma-2b": RetryProvider([ReplicateHome], shuffle=False),
             "command-r-plus": RetryProvider([HuggingChat, HuggingFace], shuffle=False),
-            "llama-3.1-70b": RetryProvider(
-                [HuggingChat, HuggingFace, Blackbox, DeepInfra, FreeGpt, TeachAnything, Free2GPT, Snova, DDG],
-                shuffle=False),
+            "llama-3.1-70b": RetryProvider([HuggingChat, HuggingFace, Blackbox, DeepInfra, FreeGpt, TeachAnything, Free2GPT, Snova, DDG], shuffle=False),
             "llama-3.1-405b": RetryProvider([Blackbox, Snova], shuffle=False),
             "llama-3.1-sonar-large-128k-online": RetryProvider([PerplexityLabs], shuffle=False),
             "llama-3.1-sonar-large-128k-chat": RetryProvider([PerplexityLabs], shuffle=False),
             "pi": RetryProvider([Pi], shuffle=False),
             "qwen-turbo": RetryProvider([Bixin123], shuffle=False),
             "qwen-2-72b": RetryProvider([Airforce], shuffle=False),
-            "mixtral-8x7b": RetryProvider([HuggingChat, HuggingFace, ReplicateHome, TwitterBio, DeepInfra, DDG],
-                                          shuffle=False),
+            "mixtral-8x7b": RetryProvider([HuggingChat, HuggingFace, ReplicateHome, TwitterBio, DeepInfra, DDG], shuffle=False),
             "mixtral-8x7b-dpo": RetryProvider([HuggingChat, HuggingFace], shuffle=False),
             "mistral-7b": RetryProvider([HuggingChat, HuggingFace, DeepInfra], shuffle=False),
             "yi-1.5-9b": RetryProvider([FreeChatgpt], shuffle=False),
             "SparkDesk-v1.1": RetryProvider([FreeChatgpt], shuffle=False),
         }
+        self.results = []
 
-        for model in providers:
-            # Replace all broken names
-            model = model.replace("3_5", "3.5")
-            model = model.replace("3_1", "3.1")
-            model = model.replace("1_5", "1.5")
-            model = model.replace("1_1", "1.1")
+    async def test_provider_availability(self):
+        sys.tracebacklimit = 0
 
-            with self.subTest(model=str(model)):
-                for provider in providers[model].providers:
-                    provider_name = provider.__name__
-                    with self.subTest(provider=provider_name):
-                        print(f"[+] Отправляю запрос провайдеру {provider_name} используя модель {model}")
-                        try:
-                            client = Client()
-                            response = client.chat.completions.create(
-                                model=model,
-                                messages=[{"role": "user", "content": "Hello"}],
-                                provider=provider
-                            )
-                            res = response.choices[0].message.content
+        tasks = []
+        for model in self.providers:
+            for provider in self.providers[model].providers:
+                tasks.append(self.check_provider(model, provider))
 
-                            # Check if response is empty
-                            var = res[0]
+        await asyncio.gather(*tasks, return_exceptions=True)
 
-                            print(f"    - Ответ от модели {model} провайдера {provider_name}: {res}")
-                        except Exception as e:
-                            if str(e) == "string index out of range":
-                                self.fail(f"Ответ от модели {model} провайдера {provider_name} пуст! ({res})")
-                            else:
-                                self.fail(
-                                    f"Ошибка при отправке запроса к модели {model} провайдера {provider_name}: {e}")
+        # Save results to a JSON file
+        with open('results.json', 'w', encoding='utf-8') as f:
+            json.dump(self.results, f, ensure_ascii=False, indent=4)
+
+    async def check_provider(self, model, provider):
+        provider_name = provider.__name__
+        print(f"[?] Отправляю запрос провайдеру {provider_name} используя модель {model}")
+        try:
+            async_create = sync_to_async(Client().chat.completions.create)
+            response = await async_create(
+                model=model,
+                messages=[{"role": "user", "content": "Hello"}],
+                provider=provider
+            )
+            res = response.choices[0].message.content
+
+            # Check if response is empty
+            var = res[0]
+
+            print(f"{self.INFO}[+] Ответ от модели {model} провайдера {provider_name}: {res}")
+            self.results.append({
+                "model": model,
+                "provider": provider_name,
+                "response": res
+            })
+        except Exception as e:
+            err = str(e)
+            msg = f"{self.ERROR}[-] Ошибка при отправке запроса к модели {model} провайдера {provider_name}: {err}"
+            if err == "string index out of range":
+                msg = f"{self.ERROR}[-] Ответ от модели {model} провайдера {provider_name} пуст! ({res})"
+
+            self.results.append({
+                "model": model,
+                "provider": provider_name,
+                "error": err
+            })
+
+            self.failureException(msg)
+            print(msg)
 
 
 if __name__ == '__main__':
