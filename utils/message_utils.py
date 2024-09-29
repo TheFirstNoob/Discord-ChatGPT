a2cb432fd982f6353d3507a285d1d1ab83ce57b5
TheFirstNoob <TheAssassin666@yandex.ru>
2024-09-29 12:59:40 +0300

    Fix a bug where after a block of code the messages were still send like code

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -3,19 +3,20 @@ from discord import Message
 async def send_split_message(self, response: str, message: Message):
     char_limit = 1900
     if len(response) > char_limit:
-        is_code_block = False
         parts = response.split("```")
+        is_code_block = False
 
         for i in range(len(parts)):
+            part = parts[i]
             if is_code_block:
-                code_block_chunks = [parts[i][j:j+char_limit] for j in range(0, len(parts[i]), char_limit)]
+                code_block_chunks = [part[j:j+char_limit] for j in range(0, len(part), char_limit)]
                 for chunk in code_block_chunks:
                     await message.followup.send(f"```{chunk}```")
             else:
-                non_code_chunks = [parts[i][j:j+char_limit] for j in range(0, len(parts[i]), char_limit)]
+                non_code_chunks = [part[j:j+char_limit] for j in range(0, len(part), char_limit)]
                 for chunk in non_code_chunks:
                     await message.followup.send(chunk)
-                is_code_block = True
+            is_code_block = not is_code_block
     else:
         await message.followup.send(response)
 
