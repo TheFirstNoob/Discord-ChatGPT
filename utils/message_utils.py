eaa8568c95c6e6e6af1cce4a636ce5ee809347f8
TheFirstNoob <TheAssassin666@yandex.ru>
2024-09-29 13:07:33 +0300

    optimize message_utils.py

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -2,20 +2,21 @@ from discord import Message
 
 async def send_split_message(self, response: str, message: Message):
     char_limit = 1900
+
+    async def send_chunks(chunk: str, is_code: bool):
+        if is_code:
+            await message.followup.send(f"```{chunk}```")
+        else:
+            await message.followup.send(chunk)
+
     if len(response) > char_limit:
         parts = response.split("```")
         is_code_block = False
 
-        for i in range(len(parts)):
-            part = parts[i]
-            if is_code_block:
-                code_block_chunks = [part[j:j+char_limit] for j in range(0, len(part), char_limit)]
-                for chunk in code_block_chunks:
-                    await message.followup.send(f"```{chunk}```")
-            else:
-                non_code_chunks = [part[j:j+char_limit] for j in range(0, len(part), char_limit)]
-                for chunk in non_code_chunks:
-                    await message.followup.send(chunk)
+        for part in parts:
+            chunks = [part[i:i + char_limit] for i in range(0, len(part), char_limit)]
+            for chunk in chunks:
+                await send_chunks(chunk, is_code_block)
             is_code_block = not is_code_block
     else:
         await message.followup.send(response)
